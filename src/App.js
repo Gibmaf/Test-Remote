257c3781f27d414468f0abb9f5704e7152d6757f
daikadai <daikadai96@gmail.com>
2022-03-15 23:32:29 +0700

    6. Adding a 'matched' Property to Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useState, useEffect } from "react";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", match: false },
+  { src: "/img/potion-1.png", match: false },
+  { src: "/img/ring-1.png", match: false },
+  { src: "/img/scroll-1.png", match: false },
+  { src: "/img/shield-1.png", match: false },
+  { src: "/img/sword-1.png", match: false },
 ];
 
 function App() {
@@ -36,7 +36,15 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log("those cards match");
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, match: true };
+            } else {
+              return card;
+            }
+          });
+        });
         resetTurn();
       } else {
         console.log("those cards do not match");
@@ -45,6 +53,7 @@ function App() {
     }
   }, [choiceOne, choiceTwo]);
 
+  console.log(cards);
   // reset choices & increase turn
   const resetTurn = () => {
     setChoiceOne(null);
